396eff1c7fa84022fe5c9bf32b406f2ecbbf3077
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2024-07-10 10:45:54 +0800

    chore: 统一插件配置存储位置，i18n文本转义换行符

--- src/main/kotlin/cn/aixcyi/plugin/dunderall/Zoo.kt
+++ src/main/kotlin/cn/aixcyi/plugin/dunderall/Zoo.kt
@@ -7,7 +7,7 @@ object Zoo {
     /**
      * 插件配置的全局存储位置。
      */
-    const val PLUGIN_STORAGE = "HooTool.DunderAll.xml"
+    const val PLUGIN_STORAGE = "HooTool.xml"
 
     /**
      * 配置所对应的 XML 的组件名。
@@ -15,7 +15,7 @@ object Zoo {
      * `<component name="NAME"></component>`
      */
     object XmlComponent {
-        const val OPTIMIZATION = "HooTool.DunderAll.Optimization"
+        const val OPTIMIZATION = "DunderAll.Optimization"
     }
 
     /** 文本资源包。 */
@@ -30,5 +30,9 @@ object Zoo {
      * @return properties 文件中键对应的值。
      */
     @JvmStatic
-    fun message(@PropertyKey(resourceBundle = "messages.PythonDunderAll") key: String): String = BUNDLE.getString(key)
+    fun message(@PropertyKey(resourceBundle = "messages.PythonDunderAll") key: String): String {
+        return BUNDLE.getString(key)
+            .replace("\\r\\n", "\n")  // "\r\n" -> LF
+            .replace("\\n", "\n")  // "\n" -> LF
+    }
 }
